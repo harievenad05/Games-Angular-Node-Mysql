bb6677eaea6ae56c547612397f3f270d4a0e6864
VidhyaRiya <vidhyariya@Haries-Mac-mini.local>
2020-04-15 21:27:50 +0300

    Client- route, navbar added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'games', pathMatch: 'full', component: GameListComponent},
+  {path: 'add-games', component: GameFormComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'games'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
